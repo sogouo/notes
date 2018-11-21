bfdd19adb8a97bd39f0363266ebbf063f162f0ab
loess90 <wf1722029940@163.com>
2018-11-21 16:31:44 +0800

    添加点东西

--- python/mydailydate.py
+++ python/mydailydate.py
@@ -18,8 +18,33 @@ class MyDailyDate():
         """ as same as: get_day_of_each_month_in_datetime
         """
         return (date.today().replace(day=1) - timedelta(days=1)).replace(day=1)
+
+    
 mydaily = MyDailyDate()
 datetime_day = mydaily.get_day_of_each_month_in_datetime()
 date_day = mydaily.get_day_of_each_month_in_date()
 print("{0} 的类型是 {1}".format(datetime_day, type(datetime_day)))
 print("{0} 的类型是 {1}".format(date_day, type(date_day)))
+
+
+""" 有时候, 通过 Model 类型按照日期字符串查询，且数据库中存储时间或者日期类型是datetime 类型,
+    客户端传入是格式化日期字符串: eg. '2018-11-21'
+    需要将日期函数转换为datetime类型: datetime.strftime(date_string, format)
+"""
+
+print("{0} 被格式化日期字符串 {1}".format(datetime_day, datetime.strftime(datetime_day,  "%Y-%m-%d")))
+
+
+
+
+"""
+    References 
+    python date of the previous month
+    https://stackoverflow.com/questions/9724906/python-date-of-the-previous-month
+
+    
+
+
+
+
+"""
